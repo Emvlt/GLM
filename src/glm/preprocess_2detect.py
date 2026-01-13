db9bb63057276c745368bb3014ade6c910ab36a7
emilien <emilienvalat@gmail.com>
2026-01-13 14:34:27 +0100

    Changed the preprocess_2detect. 1) Removed the demo file. 2) Made one file that behaves differently based on the provided parameters

--- src/glm/preprocess_2detect.py
+++ src/glm/preprocess_2detect.py
@@ -1,8 +1,10 @@
 import pathlib 
 import sys
 
+import yaml
 import numpy as np
 import imageio.v2 as imageio
+from tqdm import tqdm
 
 from odl.contrib.datasets.ct.detect import preprocess_sinogram
 
@@ -15,35 +17,45 @@ from odl.contrib.datasets.ct.detect import preprocess_sinogram
 #                     - dark.tif
 #                     - ...
 
-RAW_PATH = pathlib.Path('datasets/raw/2detect')
-PROCESSED_PATH = pathlib.Path('datasets/processed/2detect')
-
-def loop_over_dataset(mode):
-    for path_to_slice in RAW_PATH.glob(f'*/{mode}'):
+def loop_over_dataset(
+        raw_path:pathlib.Path, 
+        processed_path:pathlib.Path, 
+        mode='mode2'
+        ):
+    for path_to_slice in tqdm(raw_path.glob(f'*/{mode}')):
         slice_name = path_to_slice.parent.stem
-        path_to_processed_folder = PROCESSED_PATH.joinpath(f'{slice_name}/{mode}')
+        path_to_processed_folder = processed_path.joinpath(f'{slice_name}/{mode}')
         path_to_processed_folder.mkdir(exist_ok=True, parents=True)
         # Process the sinogram
-        path_to_processed = path_to_processed_folder.joinpath('sinogram.npy')
+        path_to_processed = path_to_processed_folder.joinpath('preprocessed_sinogram.npy')
         if not path_to_processed.is_file():
             sinogram = preprocess_sinogram(path_to_slice)
             np.save(path_to_processed, sinogram)
         # Process the reconstruction
-        path_to_processed = path_to_processed_folder.joinpath('reconstruction.npy')
+        path_to_processed = path_to_processed_folder.joinpath('preprocessed_reconstruction.npy')
         if not path_to_processed.is_file():
             reconstruction = np.asarray(imageio.imread(path_to_slice.joinpath("reconstruction.tif")))
-            np.save(PROCESSED_PATH.joinpath(f'{slice_name}/{mode}/reconstruction.npy'), reconstruction)
+            np.save(path_to_processed, reconstruction)
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 1:
         sys.stderr.write("Arguments error. Usage:\n")
-        sys.stderr.write("\tpython preprocess_2detect.py mode \n")
+        sys.stderr.write("\tpython preprocess_2detect.py mode task \n")
         sys.exit(1)
 
-    mode = sys.argv[1]
-    assert mode in ['Mode1', 'Mode2', 'Mode3'], f"Wrong mode argument, expected to be in ['Mode1', 'Mode2', 'Mode3'], got {mode}"
+    parameters = yaml.safe_load(open("params.yaml"))
 
-    loop_over_dataset(mode)
+    mode = sys.argv[1]
+    accepted_modes = ['mode1', 'mode2', 'mode3']
+    assert mode in accepted_modes, f"Wrong mode argument, expected to be in {accepted_modes}, got {mode}"
+
+    task = sys.argv[2]
+    accepted_tasks = ['demo', 'train']
+    assert task in accepted_tasks, f"Wrong task argument, expected to be in {accepted_tasks}, got {mode}"
+
+    raw_path = pathlib.Path(parameters[task]['data']['raw_path'])
+    processed_path = pathlib.Path(parameters[task]['data']['processed_path'])
+    loop_over_dataset(raw_path, processed_path, mode)
 
 if __name__ == '__main__':
     main()
