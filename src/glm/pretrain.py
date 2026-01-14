52ee5a73ca37fb58d8ddb2f5adc2fc35ce90d1df
emilien <emilienvalat@gmail.com>
2026-01-14 14:34:25 +0100

    First version of the pretraining script

---
+++ src/glm/pretrain.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+import argparse
+
+import json
+import yaml
+import torch
+from torch_geometric.data import Batch
+
+from glm.utils import plot_image
+from glm.dataset import parse_dataloader
+from glm.models.utils import (get_angles_list_from_downsampling, load_model, load_graph, load_geometry, PSNR, set_data_shape)
+
+def pretraining_loop(
+        model_save_path : Path,
+        metrics_file_path : Path
+        ):
+    # We load the different parameters
+    parameters = yaml.safe_load(open("params.yaml"))['pretrain']
+    # What are the training hyperparameters
+    hyperparameters = parameters['hyperparameters']
+    # Instanciate the device object
+    device = torch.device(hyperparameters['device'])
+    # We load the geometry object
+    downsampling = hyperparameters['downsampling']
+    angles_indices = get_angles_list_from_downsampling(downsampling)
+    n_measurements = 3600 if angles_indices is None else len(angles_indices)
+    geometry = load_geometry(angles_indices)
+    # Now the model
+    active_model = parameters['active_model']
+    model_parameters = parameters['models'][active_model]
+    model : torch.nn.Module = load_model(active_model, model_parameters)
+    model = model.to(device)
+    # And the graph
+    graph = load_graph(active_model, geometry)
+    if graph is not None:
+        graph = graph.to(device)
+        print(graph)
+    # Loss functions
+    psnr = PSNR()
+    loss_function = torch.nn.MSELoss()
+    # Datasets
+    train_dataloader = parse_dataloader(
+        dataset_path = parameters['data']['processed_path'],
+        mode = 'training',
+        data_tuples=[('preprocessed_sinogram', 'mode2')],
+        batch_size=hyperparameters['batch_size'],
+        num_workers=hyperparameters['num_workers']
+    )
+    validation_dataloader = parse_dataloader(
+        dataset_path = parameters['data']['processed_path'],
+        mode = 'validation',
+        data_tuples=[('preprocessed_sinogram', 'mode2')],
+        batch_size=hyperparameters['batch_size'],
+        num_workers=hyperparameters['num_workers']
+    )
+
+    ### Unpacking string hyperparameters
+    learning_rate = float(hyperparameters['learning_rate'])
+    epochs = int(hyperparameters['epochs'])
+    optimiser = torch.optim.Adam(
+        model.parameters(),
+        lr=learning_rate
+        )
+    
+    model_save_path = Path('glm/src/saved_models')
+
+    for epoch in range(epochs):
+        for index, tensor_dict in enumerate(train_dataloader):
+            batch_size = tensor_dict['preprocessed_sinogram_mode2'].size()[0]
+
+            input_sinogram = tensor_dict['preprocessed_sinogram_mode2'].float().to(device)
+
+            optimiser.zero_grad()
+            input_sinogram  = set_data_shape(
+                model = model,
+                batch_size=batch_size,
+                angles_indices = angles_indices,
+                tensor = input_sinogram, 
+                target='NN')
+
+            if graph is None:
+                infered_sinogram = model(input_sinogram)
+            else:
+                graphs = Batch.from_data_list([graph for sample_index in range(batch_size)] )
+                infered_sinogram = model(input_sinogram, graphs.edge_index, graphs.edge_weight)
+            
+            loss = loss_function(infered_sinogram, input_sinogram)
+            loss.backward()
+            optimiser.step()
+
+            plot_image(
+            infered_sinogram.view(batch_size, n_measurements, 956), 
+            'src/glm/images/demo/infered_sinogram', 
+            title='infered_sinogram',
+            extension='jpg'
+            )
+
+            plot_image(
+            input_sinogram.view(batch_size, n_measurements, 956), 
+            'src/glm/images/demo/input_sinogram', 
+            title='input_sinogram',
+            extension='jpg'
+            )
+            
+
+if __name__ == '__main__':
+
+    pretraining_loop()
