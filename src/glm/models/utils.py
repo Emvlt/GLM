63f713a46723d94929994ae925799aaa6b0d993e
emilien <emilienvalat@gmail.com>
2026-07-26 08:51:26 +0200

    minor type hinting

--- src/glm/models/utils.py
+++ src/glm/models/utils.py
@@ -62,7 +62,7 @@ def load_model(model_name:str, model_parameters:Dict):
     
     return model
 
-def load_geometry(angles_indices:List=None):
+def load_geometry(angles_indices:List[int]=None):
     return detect_geometry(n_voxels = 1024, angles_indices=angles_indices)
 
 def load_graph(model_name, geometry):
