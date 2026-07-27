554adc1816169b7d82b7d50a7d946b3317181e19
emilien <emilienvalat@gmail.com>
2026-07-27 17:09:06 +0200

    Adding a possibility to build a cnn/glm model with a variable number of modules and modifying the load_graph interface to use different connectivity/kernel types/ and sigma (for gaussian kernel)

--- src/glm/models/utils.py
+++ src/glm/models/utils.py
@@ -26,39 +26,42 @@ class PSNR:
             self.loss(inferred, target)
         )
 
+DEFAULT_N_MODULES = 3
+
+def _module_channel_sizes(n_channels: int, n_modules: int) -> List[Tuple[int, int]]:
+    """Returns the (in_channels, out_channels) pairs for a stack of n_modules layers
+    going from 1 channel, through n_modules - 1 hidden layers of n_channels, back to 1 channel.
+    """
+    if n_modules < 1:
+        raise ValueError(f'n_modules must be >= 1, got {n_modules}')
+    sizes = [1] + [n_channels] * (n_modules - 1) + [1]
+    return list(zip(sizes[:-1], sizes[1:]))
+
 def _build_sinogram_cnn(model_parameters: Dict) -> torch.nn.Module:
     n_channels = model_parameters['n_channels']
-    return torch.nn.Sequential(
-        CNN_Module(1, n_channels, model_parameters),
-        torch.nn.LeakyReLU(negative_slope=0.1, inplace=True),
-        CNN_Module(n_channels, n_channels, model_parameters),
-        torch.nn.LeakyReLU(negative_slope=0.1, inplace=True),
-        CNN_Module(n_channels, 1, model_parameters),
-        torch.nn.LeakyReLU(negative_slope=0.1, inplace=True),
-    )
+    n_modules = int(model_parameters.get('n_modules', DEFAULT_N_MODULES))
+
+    layers = []
+    for in_channels, out_channels in _module_channel_sizes(n_channels, n_modules):
+        layers.append(CNN_Module(in_channels, out_channels, model_parameters))
+        layers.append(torch.nn.LeakyReLU(negative_slope=0.1, inplace=True))
+    return torch.nn.Sequential(*layers)
 
 def _build_image_cnn(model_parameters: Dict) -> torch.nn.Module:
     return ImageCNN(model_parameters)
 
 def _build_glm(model_parameters: Dict) -> torch.nn.Module:
     n_channels = model_parameters['n_channels']
-    return Sequential('x, edge_index, edge_weight', [
-            (
-                GLM_Module(1, n_channels, model_parameters),
-                'x, edge_index, edge_weight -> x'
-            ),
-        torch.nn.LeakyReLU(negative_slope=0.1, inplace=True),
-            (
-                GLM_Module(n_channels, n_channels, model_parameters),
-                'x, edge_index, edge_weight -> x'
-            ),
-        torch.nn.LeakyReLU(negative_slope=0.1, inplace=True),
-            (
-                GLM_Module(n_channels, 1, model_parameters),
-                'x, edge_index, edge_weight -> x'
-            ),
-        torch.nn.LeakyReLU(negative_slope=0.1, inplace=True),
-    ])
+    n_modules = int(model_parameters.get('n_modules', DEFAULT_N_MODULES))
+
+    layers = []
+    for in_channels, out_channels in _module_channel_sizes(n_channels, n_modules):
+        layers.append((
+            GLM_Module(in_channels, out_channels, model_parameters),
+            'x, edge_index, edge_weight -> x'
+            ))
+        layers.append(torch.nn.LeakyReLU(negative_slope=0.1, inplace=True))
+    return Sequential('x, edge_index, edge_weight', layers)
 
 MODEL_REGISTRY: Dict[str, Callable[[Dict], torch.nn.Module]] = {
     'sinogram_CNN': _build_sinogram_cnn,
@@ -74,12 +77,26 @@ def load_model(model_name:str, model_parameters:Dict):
 def load_geometry(angles_indices:List[int]=None):
     return detect_geometry(n_voxels = 1024, angles_indices=angles_indices)
 
-def load_graph(model_name, geometry):
+def load_graph(
+        model_name,
+        geometry,
+        connectivity: Optional[int] = None,
+        kernel_type: Optional[str] = None,
+        sigma: Optional[float] = None,
+        ):
     if model_name == 'GLM':
+        edges_kwargs = {} if connectivity is None else {'connectivity': connectivity}
+        weights_kwargs = {}
+        if kernel_type is not None:
+            weights_kwargs['kernel_type'] = kernel_type
+        if sigma is not None:
+            weights_kwargs['sigma'] = sigma
         return create_graph_from_geometry(
             geometry = geometry,
             scheme   = 'GLM',
-            backend  = 'torch_geometric'
+            backend  = 'torch_geometric',
+            edges_kwargs = edges_kwargs,
+            weights_kwargs = weights_kwargs,
             )
     else:
         return None
